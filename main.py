f437949ae4e3e2d4aedd03088341a68ee2d33f2e
Exsul <exsul.feror@gmail.com>
2020-03-04 15:12:54 -0700

    update

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ class ConjugationGame(BoxLayout):
     game_state = NumericProperty(0)
     word1_showing = StringProperty("")
     word2_showing = StringProperty("")
+    stem1 = StringProperty("")
+    stem2 = StringProperty("")
+    answer1 = StringProperty("")
+    answer2 = StringProperty("")
 
     # FIRST VERB
     verb1_lex = "laudo, laudāre, laudavi, laudatum, to praise"
@@ -46,6 +50,13 @@ class ConjugationGame(BoxLayout):
     word1_selected = word1_shuffled_list.pop()
     word2_selected = word2_shuffled_list.pop()
 
+    def change_round(self):
+        # CHANGE ROUND
+        if self.round > 3:
+            self.round = 1
+        else:
+            self.round += 1
+
     def change_game_state(self):
         # CHANGE GAME STATE
         if self.game_state > 3:  # at end, pop next item
@@ -62,33 +73,49 @@ class ConjugationGame(BoxLayout):
             else:
                 self.word2_shuffled_list = shuffle_list(self.word2)
                 self.word2_selected = self.word2_shuffled_list.pop()
-
         else:  # advance game state
             self.game_state += 1
+
+        # DECIDE WHICH ASPECT TO SHOW
+        if self.round == 1:  # TRANSLATION
+            self.stem1 = self.word1_selected[-2]
+            self.stem2 = self.word2_selected[-2]
+            self.answer1 = self.word1_selected[-1]
+            self.answer2 = self.word2_selected[-1]
+        elif self.round == 2:  # PARSING
+            self.stem1 = self.word1_selected[-2]
+            self.stem2 = self.word2_selected[-2]
+            self.answer1 = self.word1_selected[0] + "\n" + \
+                           self.word1_selected[1] + " person \n" + \
+                           self.word1_selected[2]
+            self.answer2 = self.word2_selected[0] + "\n" + \
+                           self.word2_selected[1] + " person \n" + \
+                           self.word2_selected[2]
+        else:  # self.round == 3: CONJUGATION
+            pass
 
         # DECIDE WHICH WORD TO SHOW
         if self.game_state == 0:
             self.word1_showing = ""
             self.word2_showing = ""
         elif self.game_state == 1:
-            self.word1_showing = self.word1_selected[-2]
+            self.word1_showing = self.stem1
             self.word2_showing = ""
         elif self.game_state == 2:
-            self.word1_showing = self.word1_selected[-2] + "\n" + self.word1_selected[-1]
+            self.word1_showing = self.stem1 + "\n" + self.answer1
             self.word2_showing = ""
         elif self.game_state == 3:
             self.word1_showing = ""
-            self.word2_showing = self.word2_selected[-2]
+            self.word2_showing = self.stem2
         elif self.game_state == 4:
             self.word1_showing = ""
-            self.word2_showing = self.word2_selected[-2] + "\n" + self.word2_selected[-1]
-
+            self.word2_showing = self.stem2 + "\n" + self.answer2
 
 
 class ConjugationApp(App):
     def build(self):
         Window.bind(on_key_down=self.key_down)
-        Window.bind(on_key_up=self.key_up)
+        # Window.bind(on_key_up=self.key_up)
         return ConjugationGame()
 
     def key_down(self, key, scancode=None, *_):
@@ -99,18 +126,19 @@ class ConjugationApp(App):
             print("PAGE_UP pressed")
         elif scancode == 98:  # B
             print("B pressed")
+            ConjugationGame.change_round(self.root)
         else:
             print("\nKey: {} (scancode {}) pressed".format(_[1], scancode))
 
-    def key_up(self, key, scancode=None, *_):
-        if scancode == 281:  # PAGE_DOWN
-            print("PAGE_DOWN released")
-        elif scancode == 280:  # PAGE_UP
-            print("PAGE_UP released")
-        elif scancode == 98:  # B
-            print("B released")
-        else:
-            print("\nKey: {} (scancode {}) released".format(_, scancode))
+    # def key_up(self, key, scancode=None, *_):
+    #     if scancode == 281:  # PAGE_DOWN
+    #         print("PAGE_DOWN released")
+    #     elif scancode == 280:  # PAGE_UP
+    #         print("PAGE_UP released")
+    #     elif scancode == 98:  # B
+    #         print("B released")
+    #     else:
+    #         print("\nKey: {} (scancode {}) released".format(_, scancode))
 
 
 if __name__ == "__main__":
